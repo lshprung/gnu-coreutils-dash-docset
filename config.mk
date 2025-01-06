b52dd9f25fb55a29314db46c762bad9d47e3e09e
Louie Shprung <lshprung@tutanota.com>
2025-01-06 15:01:23 -0500

    Convert to submodule; python rewrite

---
+++ config.mk
@@ -0,0 +1,17 @@
+SRC_ICON_FILE = $(SOURCE_DIR)/icon.png
+INDEX_ENTRY_CLASS=printindex-index-entry
+
+MANUAL_URL  = https://www.gnu.org/software/coreutils/manual/coreutils.html_node.tar.gz
+MANUAL_FILE = tmp/coreutils.html_node.tar.gz 
+
+$(MANUAL_FILE): tmp
+	curl -o $@ $(MANUAL_URL)
+
+$(DOCUMENTS_DIR): $(RESOURCES_DIR) $(MANUAL_FILE)
+	mkdir -p $@
+	tar -x -z -f $(MANUAL_FILE) -C $@
+
+$(INDEX_FILE): $(SOURCE_DIR)/src/index-pages.py $(SCRIPTS_DIR)/gnu/index-terms.py $(DOCUMENTS_DIR)
+	rm -f $@
+	$(SOURCE_DIR)/src/index-pages.py $@ $(DOCUMENTS_DIR)/*.html
+	$(SCRIPTS_DIR)/gnu/index-terms.py Entry $(INDEX_ENTRY_CLASS) $@ $(DOCUMENTS_DIR)/Concept-index.html
